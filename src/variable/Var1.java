b9ce90be1aee27c7de8554d7c7ec06d06d4f894a
yuwankang <kangyuwan@naver.com>
2024-01-14 19:43:17 +0900

    변수 선언 및 초기화

---
+++ src/variable/Var1.java
@@ -0,0 +1,11 @@
+package variable;
+
+import javax.sound.midi.Soundbank;
+
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println(20);
+        System.out.println(20);
+        System.out.println(20);
+    }
+}
