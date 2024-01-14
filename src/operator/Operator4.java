563901616a6f41eb42c0415c2f243839d0971be2
yuwankang <kangyuwan@naver.com>
2024-01-14 20:53:55 +0900

    연산자 우선순위

---
+++ src/operator/Operator4.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class Operator4 {
+    public static void main(String[] args) {
+        //연산자 우선순위
+        // 1순위 () 괄호
+        // 2순위 단항 연산자 ++,--,!,~,new,(type)
+        // 개발에서 우선순위는 상식선 - 애매하면 괄호를 사용한다.
+        // 누구나 보고쉽게 하는게 좋은 개발이다. 단순함과 명확암 애매하거나 복잡함 x
+    }
+}
