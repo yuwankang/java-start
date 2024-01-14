fe4e114dc9bf26a15ccb3be1228d834c1d13003b
yuwankang <kangyuwan@naver.com>
2024-01-14 20:06:57 +0900

    변수 명명 과 규칙

---
+++ src/variable/Var8.java
@@ -0,0 +1,23 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        // 정수
+        byte b = 127; //-128~127
+        short s = 32767; // - 32,768 ~ 32,767
+        int i = 2147483647; // -21억~21억
+
+        long l = 9223372036854775807L; //엄청 큰 크기
+
+        //실수
+        float f = 10.0f;
+        double d = 10.0;
+        // byte, short, float, char 잘사용하지 않는다
+        // 변수 명명 규칙
+        // 변수의 이름은 숫자로 시작 불가능
+        // 이름에는 공백이 들어갈 수 없다.
+        // 변수는 소문자로 시작하는 낙타 표기법  orderDetail, myAccount
+        // 상수는 모두 대문자 패키지는 소문자 클래스 - 첫 글자는 대문자 나머지는 소문자 + 낙타 표기법
+
+    }
+}
