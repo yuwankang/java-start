692e34b0a4bc3cd58192916f782eaae14ef6aeaa
yuwankang <kangyuwan@naver.com>
2024-01-14 22:49:31 +0900

    연산자 공부 및 문제풀이

---
+++ src/operator/ex/OperationEx2.java
@@ -0,0 +1,15 @@
+package operator.ex;
+
+public class OperationEx2 {
+    public static void main(String[] args) {
+        double val1 = 1.5;
+        double va12 = 2.5;
+        double val3 = 3.5;
+
+        double sum = val1+va12+val3;
+        double average = sum/3;
+        System.out.println(sum);
+        System.out.println(average);
+
+    }
+}
