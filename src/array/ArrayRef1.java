45e6f39fec892a79d05deabc6fd32c27c0ad1d13
yuwankang <kangyuwan@naver.com>
2024-01-29 22:48:55 +0900

    배열 리팩토링

--- src/array/ArrayRef1.java
+++ src/array/ArrayRef1.java
@@ -4,7 +4,6 @@ public class ArrayRef1 {
     public static void main(String[] args) {
         int[] students; //배열 변수 선언
         students = new int[5];
-
         //변수 값 대입
         students[0] = 90;
         students[1] = 80;
